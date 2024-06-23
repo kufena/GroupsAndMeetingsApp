f8485713e80cda13fb7bfa34cc9ad6af3092d912
Andrew Douglas <fairtomiddlingster@gmail.com>
2024-06-23 13:10:52 +0100

    just noted what to do to get tokens.

--- FrontEnd/Pages/SignUp.cshtml.cs
+++ FrontEnd/Pages/SignUp.cshtml.cs
@@ -5,8 +5,20 @@ namespace FrontEnd.Pages
 {
     public class SignUpModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(string code)
         {
-        }
+            Console.WriteLine("Code: " + code);
+
+            // POST to oauth2/token with client_id, client_secret, code, grant_type
+            // Authorization: Basic base64(client_id:client_secret) possible.
+            // returns:
+            // { 
+            //                "access_token":"eyJra1example", 
+            //                "id_token":"eyJra2example",
+            //                "refresh_token":"eyJj3example",
+            //                "token_type":"Bearer", 
+            //                "expires_in":3600
+            //               }
+    }
     }
 }
